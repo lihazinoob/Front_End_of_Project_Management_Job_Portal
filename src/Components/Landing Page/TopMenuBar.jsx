6a4d724d379546829b678e1f75ea6098f8946b04
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-12-04 06:20:52 +0600

    The Design of the topmenubar is completed

--- src/Components/Landing Page/TopMenuBar.jsx
+++ src/Components/Landing Page/TopMenuBar.jsx
@@ -2,23 +2,32 @@ import React from 'react'
 
 const TopMenuBar = () => {
   return (
-    <div className='bg-topmenubarbgcolor'>
-      <div className='text-white p-4 h-18 flex flex-row items-end  gap-96 ml-16'>
-        <div>
+    <div className='bg-topmenubarbgcolor rounded-sm shadow-lg shadow-black'>
+      <div className='text-white p-4 h-18 flex flex-row items-end  gap-64 ml-16'>
+        <div className='font-bold tracking-widest text-2xl'>
           ProJob
         </div>
         <div>
-          <nav className='flex flex-row items-center gap-24'>
-            <div>
+          <nav className='flex flex-row items-center gap-24 tracking-wide text-base'>
+            <div className='text-center cursor-pointer transition-transform tr3ransform hover:-translate-y-1 hover:shadow-lg'>
+              
               Explore Jobs
             </div>
-            <div>
+            <div className='text-center cursor-pointer transition-transform transform hover:-translate-y-1 hover:shadow-lg'>
               Explore Companies
             </div>
-            <div>
+            <div className='text-center cursor-pointer transition-transform transform hover:-translate-y-1 hover:shadow-lg'>
               Testimonials
             </div>
           </nav>
+        </div>
+        <div className='flex flex-row items-center gap-12'>
+          <button className='text-center cursor-pointer transition-transform transform hover:-translate-y-1 hover:shadow-lg hover:text-lg'>
+            Login
+          </button>
+          <button className='text-center cursor-pointer transition-transform transform hover:-translate-y-1 hover:shadow-lg hover:text-lg'>
+            SignUp
+          </button>
         </div>
       </div>
     </div>
